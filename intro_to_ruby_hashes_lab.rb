a6476ec4ac39ab0dae6a48590ae46ff0ccb5467e
Paul Leugemors <pleugemors@yahoo.com>
2019-10-17 05:11:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads =>{ :pieces => 4, 
     :rent_in_dollars => {:one_piece_owned => 25,
-      :key2 => 2 ,
+      :two_pieces_owned => 50 ,
       :key3 => 3 ,
       :key4 => 4 
       },
