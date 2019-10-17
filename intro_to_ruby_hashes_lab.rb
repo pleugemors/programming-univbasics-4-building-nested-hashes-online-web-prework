eae32769bbb6e3df12611c26ba1ce5eea755adf4
Paul Leugemors <pleugemors@yahoo.com>
2019-10-17 05:14:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
       },
     :names => {:reading_railroad => {},
       :pennsylvania_railroad => {},
-      :key3 => 3,
+      :b_and_o_railroad => {},
       :key4 => 4
       }
    }
